898be97350f351ac91744529768d2cc1ddad14b9
Kevin F <Freymaurer@gmx.de>
2019-05-20 23:24:06 +0200

    test for documentation

--- docsrc/content/PathfinderWebApp.fsx
+++ docsrc/content/PathfinderWebApp.fsx
@@ -11,6 +11,7 @@ Web Application for PathfinderAttackSimulator
 Calculate Damage Per Round
 -------
 
+![PathfinderAppOverview](C:\Users\Freym\source\repos\Freymaurer\PathfinderAttackSimulator\docs\content\img\PathfinderWebApp1.PNG)
 This tool is meant to help players compare different builds in their damage output per round.
 
 *)
