cd6de039f2572ea4b09834f16600ee9c5da5dd0c
Kevin F <Freymaurer@gmx.de>
2021-07-23 22:26:15 +0200

    Add first set of base types

---
+++ src/Pathician/Types/ComplexTypes.fs
@@ -0,0 +1,43 @@
+﻿namespace Pathician.Types
+
+type Dice = {
+    n:      int
+    Dice:   int
+} with
+    member this.roll() =
+        Pathician.DiceRoller.rollDice this.n this.Dice
+    member this.rollSum() =
+        Pathician.DiceRoller.rollDice this.n this.Dice
+        |> Array.sum
+
+type Bonus = {
+    Bonus: int
+    BonusType: UnionTypes.Bonus.Types
+}
+
+module ComplexTypes =
+    
+    type DamageDice = {
+        /// How many which sided dice need to be rolled for damage
+        Damage:             Dice option
+        /// Flat Boni to damage, stacking and non-stacking
+        DamageBonus:        int option
+        /// damage type (e.g. fire, slashing, force)
+        DamageType:         UnionTypes.DamageTypes
+    } with
+        member this.rollSum() =
+            match this.Damage, this.DamageBonus with
+            | Some dmg, Some flatBonus  -> dmg.rollSum() + flatBonus
+            | Some dmg, None            -> dmg.rollSum()
+            | None, Some flatBonus      -> flatBonus
+            | None, None                -> 0
+                
+    type Damage = {
+        DamageDice:         DamageDice
+        // TODO: Might not be necessary
+        /// if the Damage is multiplied on crit
+        MultipliedOnCrit:   bool
+        /// if damage crits, how often is it multiplied
+        CritMultiplier:     int
+        OnCritDamage:       DamageDice option
+    }
---
+++ src/Pathician/Types/UnionTypes.fs
@@ -0,0 +1,103 @@
+﻿namespace Pathician.Types
+
+open System
+
+/// This module is made from several sub moduls containing necessary types to create characters, weapons and modifications, as well as librarys for each of those classes.
+module UnionTypes =
+    
+    type SizeType =
+    | Fine
+    | Diminuitive
+    | Tiny
+    | Small
+    | Medium
+    | Large
+    | Huge
+    | Gargantuan
+    | Colossal 
+        static member ofString (str:string) =
+            match str with
+            | "Fine"        -> Fine
+            | "Diminuitive" -> Diminuitive
+            | "Tiny"        -> Tiny
+            | "Small"       -> Small
+            | "Medium"      -> Medium
+            | "Large"       -> Large
+            | "Huge"        -> Huge
+            | "Gargantuan"  -> Gargantuan
+            | "Colossal"    -> Colossal
+            | anythingElse  -> failwith (sprintf "Error. Could not parse SizeType of `%s`!" anythingElse)
+
+    type AbilityScore =
+    | Strength
+    | Dexterity
+    | Constitution
+    | Intelligence
+    | Wisdom
+    | Charisma
+        static member ofString (str:string) =
+            match str with
+            | "Strength"        -> Strength
+            | "Dexterity"       -> Dexterity
+            | "Constitution"    -> Constitution
+            | "Intelligence"    -> Intelligence
+            | "Wisdom"          -> Wisdom
+            | "Charisma"        -> Charisma
+            | anythingElse  -> failwith (sprintf "Error. Could not parse AbilityScore of `%s`!" anythingElse)
+
+    type DamageTypes =
+        | Fire
+        | Cold
+        | Acid
+        | Electricity
+        | Bludgeoning
+        | Slashing
+        | Piercing
+        | BludgeoningOrPiercing
+        | BludgeoningOrPiercingOrSlashing
+        | PiercingOrSlashing
+        | PositiveEnergy
+        | NegativeEnergy
+        | Force
+        | Precision
+        | Untyped
+
+    module Bonus =
+
+        /// StackingBonusTypes are Pathfinder Bonus types which stack. Not only the highest bonus counts.
+        type StackingBonusTypes =
+        | Circumstance
+        
+        /// UniqueBonusTypes are Pathfinder Bonus types which do not stack. Only the highest bonus counts.
+        type UniqueBonusTypes =
+        | Alchemical
+        | Competence
+        | Enhancement
+        | Inherent
+        | Insight
+        | Luck
+        | Morale
+        | Racial
+        | Profane
+        | Sacred
+        | Size
+        | Trait
+
+        type Types =
+        | StackingBonus of StackingBonusTypes
+        | UniqueBonus of UniqueBonusTypes 
+
+    module BonusAttacks =
+
+        type UniqueBonusAttacks =
+        | HasteLike
+        | TWFLike
+        | FlurryOfBlowsLike
+
+        type Types =
+        | StackingBonusAttacks
+        | UniqueBonus of UniqueBonusAttacks
+
+
+
+
