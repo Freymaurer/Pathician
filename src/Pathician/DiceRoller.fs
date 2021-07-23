006195ec3f7fb59eea2b5628fbf6aee01023687f
Kevin F <Freymaurer@gmx.de>
2021-07-23 22:26:01 +0200

    Add dice roller

---
+++ src/Pathician/DiceRoller.fs
@@ -0,0 +1,9 @@
+﻿namespace Pathician
+
+module DiceRoller =
+
+    let rollDice count (diceSides:int) =
+        let rnd = System.Random()
+        if diceSides = 0 
+        then [|0|]
+        else Array.init count (fun _ -> rnd.Next (1, diceSides+1))
